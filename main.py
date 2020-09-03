1a7da5399ddeb79f2877ad5f43aac37eeacb657f
spookyghostyy <69911324+spookyghostyy@users.noreply.github.com>
2020-09-03 17:29:04 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ info.set_score(0)
 info.set_life(3)
 
 #Setup the player 
+spaceship = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . a a . . . . . . .
+    . . . . a a a a a a a . . . . .
+    . . . . a a a a a a a a a a a .
+    . . . . a a a a a a a a . . . .
+    . . . a a a a . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+"""))
+spaceship.set_position(10, 60)
+spaceship.set_flag(SpriteFlag.STAY_IN_SCREEN, True )
 
 #Configure player controls 
+controller.move_sprite(spaceship, 200,200)
 
 #Make Enemys
 
--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
 // Setup the game
 info.setScore(0)
 info.setLife(3)
+// Setup the player 
+let spaceship = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . a a . . . . . . .
+    . . . . a a a a a a a . . . . .
+    . . . . a a a a a a a a a a a .
+    . . . . a a a a a a a a . . . .
+    . . . a a a a . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`)
+spaceship.setPosition(10, 60)
+spaceship.setFlag(SpriteFlag.StayInScreen, true)
+// Configure player controls 
+controller.moveSprite(spaceship, 200, 200)
